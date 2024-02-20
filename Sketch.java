a112f042f294dae6000323544f06d5e47d2935a8
Erick-Meza <erick.mezavilleda25@ycdsbk12.ca>
2024-02-20 17:27:36 -0500

    Completed Sketch Assignment

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(600, 400);
+    size(620, 400);
   }
 
   /** 
@@ -16,30 +16,39 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(20, 201, 159);
+    background(0, 0, 0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+
+	// defines the thickness of the lines/shapes
+    strokeWeight(12);
+
 	// draws red equilateral triangle on the left
     stroke(255,0,0);
-    fill(20, 201, 159);
-    triangle(80, 300, 80, 80, 220, 200);
+    fill(0,0,0,0);
+    triangle(90, 310, 90, 70, 270, 200);
+
+  // draws a blue diamond on the right side
+    stroke(0,0,255);
+    quad(320, 200, 440, 70, 555, 200, 440, 304);
   
-  // draws grey ellipse in the center
-    stroke(100);
-    tint(255,127);
-    ellipse(270, 190, 250, 220);
+  // draws a white ellipse in the center
+    stroke(255,255,255);
+    fill(0,0,0,0);
+    ellipse(300, 190, 300, 260);
 
+  // draws a blue line in front of the ellipse for the diamond
+    stroke(0,0,255);
+    line(320, 198, 440, 70);
+  
+  // draws a red line in front of the ellipse for the triangle
     stroke(255,0,0);
-    strokeWeight(16);
-    line(153,142,250,300);
-
-
- 
+    line(90, 70, 270, 200);
+    
   }
   
   // define other methods down here.
